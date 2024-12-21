90be2b9ecc88ba916264c28b840e8f1c9fc33cda
ionztorm <leon.codes@icloud.com>
2024-12-21 17:41:15 +0000

    add trouble, load colour scheme earlir

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,34 +1,55 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  {
-    change_detection = {
-        enabled = true,
-        notify = false,
-    }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  -- checker = { enabled = true },
+	spec = {
+		{
+			"folke/tokyonight.nvim",
+			lazy = false,
+			priority = 1000,
+			config = function()
+				require("tokyonight").setup({
+					style = "night",
+					transparent = true,
+					terminal_colors = true,
+					styles = {
+						floats = "transparent",
+						sidebars = "transparent",
+						comments = { italic = false },
+						keywords = { italic = false },
+						variables = { bold = true },
+					},
+					dim_inactive = true,
+				})
+				vim.cmd.colorscheme("tokyonight")
+			end,
+		},
+
+		{ import = "plugins" },
+	},
+	{
+		change_detection = {
+			enabled = true,
+			notify = false,
+		},
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	-- install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	-- checker = { enabled = true },
 })
